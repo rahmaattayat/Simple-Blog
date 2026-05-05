5b134c84933e8e97bdb4f1a6eb328a3a384025ae
rahmaattayat <rahmaattaya015@gmail.com>
2026-05-06 02:51:00 +0700

    DTO

---
+++ SoftwareTesting Junit/Spring-Simple Blog/source code/src/main/java/com/blog/dto/CommentRequest.java
@@ -0,0 +1,32 @@
+package com.blog.dto;
+
+public class CommentRequest {
+
+	private Long postId;
+	private String user;
+	private String comment;
+
+	public Long getPostId() {
+		return postId;
+	}
+
+	public void setPostId(Long postId) {
+		this.postId = postId;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+}
---
+++ SoftwareTesting Junit/Spring-Simple Blog/source code/src/main/java/com/blog/dto/PostRequest.java
@@ -0,0 +1,41 @@
+package com.blog.dto;
+
+public class PostRequest {
+
+	private Long id;
+	private String user;
+	private String title;
+	private String content;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+}
