0bd502bb2ab8fb7adb5f9f43238950d179f807e7
zahraldila <zaraliskandar@gmail.com>
2026-05-05 14:59:42 +0700

    Fase 1: corrective active postcontroller dan commentcontroller

---
+++ src/main/java/com/blog/service/CommentService.java
@@ -0,0 +1,52 @@
+package com.blog.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.blog.repository.CommentJpaRepository;
+import com.blog.vo.Comment;
+
+@Service
+public class CommentService {
+
+	@Autowired
+	CommentJpaRepository commentJpaRepository;
+	
+	public boolean  saveComment(Comment comment) {
+		Comment result = commentJpaRepository.save(comment);
+		boolean isSuccess = true;
+		
+		if(result == null) {
+			isSuccess = false;
+		}
+		
+		return isSuccess;
+	}
+
+	public List<Comment> getCommentList(Long postId) {
+		List<Comment> postList = commentJpaRepository.findAllByPostIdOrderByRegDateDesc(postId);
+		return postList;
+	}
+	
+	public List<Comment> searchCommentList(Long postId, String query) {
+		List<Comment> postList = commentJpaRepository.findByPostIdAndCommentContainingOrderByRegDateDesc(postId, query);
+		return postList;
+	}
+
+	public Comment getComment(Long id) {
+		Comment comment = commentJpaRepository.findOneById(id);
+		return comment;
+	}
+
+	public boolean deleteComment(Long id) {
+		Comment result = commentJpaRepository.findOneById(id);
+		
+		if(result == null)
+			return false;
+		
+		commentJpaRepository.deleteById(id);
+		return true;
+	}
+}
